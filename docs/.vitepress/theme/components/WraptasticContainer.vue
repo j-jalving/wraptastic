240f2ab60f408b80894a7dfa1d123630e93e5873
Jouri Jalving <info@jourijalving.nl>
2023-05-28 17:47:27 +0200

    Clean up and improve examples page and components

--- docs/.vitepress/theme/components/WraptasticContainer.vue
+++ docs/.vitepress/theme/components/WraptasticContainer.vue
@@ -1,37 +1,57 @@
 <template>
-  <div class="wraptastic-container">
-    <vue-wraptastic
-      :container="config.container"
-      :data="config.data"
-      :lines="config.lines"
-      :inline="config.inline"
-      :item="config.item"
-      :itemClass="config.itemClass"
-      :counter="config.counter"
-      :counterClass="config.counterClass"
-      :counterTemplate="config.counterTemplate"
-      :counterEnabled="config.counterEnabled"
-    >
-      <slot></slot>
-    </vue-wraptastic>
+  <div class="wraptastic-container" :class="classNames">
+    <slot></slot>
   </div>
+  <a href="#" class="toggle" @click="animate = !animate" v-if="animateButton">
+    {{ animate ? "Stop demo" : "Start demo" }}</a
+  >
 </template>
 
-<script>
-import VueWraptastic from "./VueWraptastic.vue";
-
+<script type="ts">
 export default {
-  components: {
-    VueWraptastic,
-  },
-  data() {
-    return {};
-  },
   props: {
-    config: {
-      type: Object,
-      default: () => {},
+    animate: {
+      type: Boolean,
+      default: false
     },
+    animateButton: {
+      type: Boolean,
+      default: true
+    }
   },
+  computed: {
+    classNames() {
+      return {
+        "animate": this.animate
+      }
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+.wraptastic-container {
+  min-width: 20%;
+  max-width: 100%;
+  min-height: 150px;
+  margin: 16px 0;
+  padding: 20px;
+  overflow: hidden;
+  border: 1px solid var(--vp-c-divider);
+  border-radius: 8px;
+  resize: horizontal;
+}
+
+.animate {
+  animation: stretch 6s alternate infinite 1s ease-in-out;
+}
+
+@keyframes stretch {
+  0% {
+    width: 100%;
+  }
+  100% {
+    width: 20%;
+  }
+}
+</style>
